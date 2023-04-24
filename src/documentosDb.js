cfd55e3defaecc9e5f0adcbcdd845d71fe5006ac
Igor Martins <igrmartoli@gmail.com>
2023-04-24 10:23:26 -0300

    Inserindo links dinamicamente, obtendo documentos do mongodb

--- src/documentosDb.js
+++ src/documentosDb.js
@@ -1,5 +1,11 @@
 import { documentosColecao } from "./dbConnect.js";
 
+function obterDocumentos(){
+  const documentos = documentosColecao.find().toArray();
+  
+  return documentos;
+}
+
 function encontrarDocumento(nome) {
   const documento = documentosColecao.findOne({
     nome,
@@ -23,4 +29,4 @@ function atualizaDocumento(nome, texto) {
   return atualizacao;
 }
 
-export { encontrarDocumento, atualizaDocumento };
+export { encontrarDocumento, atualizaDocumento, obterDocumentos };
